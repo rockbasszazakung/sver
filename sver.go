9052fa5a9d31d776eb77b6d854a8eb2078d43626
wanatthaphong.p <you@example.com>
2021-03-06 14:34:21 +0700

    portPost

--- sver.go
+++ sver.go
@@ -15,7 +15,7 @@ func helloHandler(c echo.Context) error {
 
 func main() {
 	e := echo.New()
-	e.GET("/hello", helloHandler)
+	e.POST("/hello", helloHandler)
 	port := os.Getenv("PORT")
 	//log.Print("Port", port)
 	e.Start(":" + port)
