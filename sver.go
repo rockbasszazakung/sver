4897d19c6ea860dc5384b60ef291425d871e688c
wanatthaphong.p <you@example.com>
2021-03-13 09:18:57 +0700

    home2

--- sver.go
+++ sver.go
@@ -52,13 +52,13 @@ func puthello(c echo.Context) error {
 	return c.JSON(http.StatusCreated, "PUT todo")
 }
 func puthello2(c echo.Context) error {
-	// t := new(todos)
-	// if err := c.Bind(t); err != nil {
-	// 	return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
-	// }
-	// id, _ := strconv.Atoi(c.Param("id"))
-	// todos[id].Title = t.Title
-	// todos[id].Status = t.Status
+	t := new(Todo)
+	if err := c.Bind(t); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	todos[id].Title = t.Title
+	todos[id].Status = t.Status
 	return c.JSON(http.StatusCreated, "PUT todo2")
 }
 
